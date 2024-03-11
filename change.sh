4e17116ac0df85d5dc79da31bbfbca5b6629c7ca
ruchid <ruchid@protonmail.com>
2024-03-11 21:08:25 +0800

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-# 编辑 mirrorlist 文件以添加清华、阿里和中科大的镜像源
-sudo tee -a /etc/pacman.d/mirrorlist > /dev/null <<EOF
-# 清华源
-Server = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
-# 阿里源
-Server = http://mirrors.aliyun.com/archlinux/\$repo/os/\$arch
-# 中科大源
-Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch
-EOF
-
-
-# 更新 pacman 数据库并升级系统
-sudo pacman -Syyu
+ pacman-mirrors -i -c China -m rank 
